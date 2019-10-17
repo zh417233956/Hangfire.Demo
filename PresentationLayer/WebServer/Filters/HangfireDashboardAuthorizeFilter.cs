14724c182abe02c8bdb58ea9940e4f50686e93ca
钟海 <417233956@qq.com>
2019-10-17 14:46:49 +0800

    server端第一步实现

---
+++ PresentationLayer/WebServer/Filters/HangfireDashboardAuthorizeFilter.cs
@@ -0,0 +1,23 @@
+﻿using Hangfire.Annotations;
+using Hangfire.Dashboard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebServer.Filters
+{
+    /// <summary>
+    /// Hangfire面板管理权限
+    /// </summary>
+    public class HangfireDashboardAuthorizeFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize([NotNull] DashboardContext context)
+        {
+            //var httpcontext = context.GetHttpContext();
+            //return httpcontext.User.Identity.IsAuthenticated;
+            return true;
+        }
+
+    }
+}
--- PresentationLayer/WebServer/Startup.cs
+++ PresentationLayer/WebServer/Startup.cs
@@ -2,19 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Hangfire;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+using WebServer.Filters;
 
 namespace WebServer
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            #region Hangfire配置
+            string HangfireCon = Configuration.GetConnectionString("Hangfire.Redis");
+            var RedisOptions = ConfigurationOptions.Parse(HangfireCon);
+            var HangfireRedis = ConnectionMultiplexer.Connect(RedisOptions);
+
+            services.AddHangfire(configuration =>
+            {
+                configuration.UseRedisStorage(HangfireRedis, new Hangfire.Redis.RedisStorageOptions() { Db = (int)RedisOptions.DefaultDatabase, Prefix = "{hangfire}:Mr.right:" });
+            });
+            #endregion Hangfire配置
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +48,19 @@ namespace WebServer
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+
+            app.UseHangfireServer();
+
+            app.UseHangfireDashboard("/hangfire", new DashboardOptions()
             {
-                await context.Response.WriteAsync("Hello World!");
+                Authorization = new[] { new HangfireDashboardAuthorizeFilter() }
             });
+
+            app.UseMvc();
         }
     }
 }
