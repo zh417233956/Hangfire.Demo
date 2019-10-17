22e73d6ebc2507e187bec74522aeeb73d71901a9
钟海 <417233956@qq.com>
2019-10-17 18:24:46 +0800

    client端第一步实现

---
+++ PresentationLayer/WebClient/API/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebClient.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        ILogger<ValuesController> logger;
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            this.logger = logger;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            logger.LogWarning("开始设置延迟作业,1分钟以后执行");
+
+            var jobId = Hangfire.BackgroundJob.Schedule<TaskService>(t=>t.TestMethod(DateTime.Now), TimeSpan.FromMinutes(1));
+
+            return jobId;
+        }
+    }
+    public class TaskService 
+    {
+        ILogger<TaskService> logger;
+        public TaskService(ILogger<TaskService> logger)
+        {
+            this.logger = logger;
+        }
+        public void TestMethod(DateTime datetime)
+        {
+            //记录日志
+            logger.LogWarning($"创建时间:{datetime.ToString()}");
+        }
+    }
+}
---
+++ PresentationLayer/WebClient/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hangfire;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
+
+namespace WebClient
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            #region Hangfire配置
+            string HangfireCon = Configuration.GetConnectionString("Hangfire.Redis");
+            var RedisOptions = ConfigurationOptions.Parse(HangfireCon);
+            var HangfireRedis = ConnectionMultiplexer.Connect(RedisOptions);
+
+            services.AddHangfire(configuration =>
+            {
+                configuration.UseRedisStorage(HangfireRedis, new Hangfire.Redis.RedisStorageOptions() { Db = (int)RedisOptions.DefaultDatabase, Prefix = "{hangfire}:Mr.right:" });
+            });
+            #endregion Hangfire配置
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddTransient<API.TaskService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            loggerFactory.AddLog4Net("Configs/log4net.config");
+
+            app.UseHangfireServer();
+
+            //app.UseHangfireDashboard("/hangfire");
+
+            app.UseMvc();
+
+        }
+    }
+}
